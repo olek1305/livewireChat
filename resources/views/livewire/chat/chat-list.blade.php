3a52a545eacc9686a289f476d031fd17d919f0e7
olek1305 <olek1305@gmail.com>
2024-04-28 23:28:25 +0200

    Added test div for css

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,9 +1,18 @@
 <div>
     <div class="chat_list_header">
 
+        <div class="title">
+            Chat
+        </div>
+
+        <div class="img_container">
+            <img src="https://picsum.photos/id/237/200/300" alt="dog">
+        </div>
+
     </div>
 
     <div class="chat_list_body">
-
+        <p>Test</p>
     </div>
+
 </div>
