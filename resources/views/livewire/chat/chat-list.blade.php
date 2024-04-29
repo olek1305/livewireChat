e81c4f9cf5eabc49de217a29a3eb898cca65ee14
olek1305 <olek1305@gmail.com>
2024-04-29 19:37:53 +0200

    added better css

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,6 +1,5 @@
 <div>
     <div class="chat_list_header">
-
         <div class="title">
             Chat
         </div>
@@ -8,11 +7,33 @@
         <div class="img_container">
             <img src="https://picsum.photos/id/237/200/300" alt="dog">
         </div>
-
     </div>
 
     <div class="chat_list_body">
-        <p>Test</p>
-    </div>
 
+        <div class="chat_list_item">
+            <div class="chat_list_img_container">
+                <img src="https://picsum.photos/id/200/200/300" alt="dog">
+            </div>
+
+            <div class="chat_list_info">
+                <div class="top_row">
+                    <div class="list_username">John</div>
+                    <span class="date">2d</span>
+                </div>
+
+                <div class="bottom_row">
+                    <div class="message_body truncate">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam debitis dicta doloribus eaque error eum inventore, itaque mollitia nihil nulla pariatur quae quas quisquam reiciendis tempore ullam vel voluptatum.
+                    </div>
+
+                    <div class="unread_count">
+                        56
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
 </div>
