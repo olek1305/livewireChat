223520366e5829de51f6eff267629ddc6b6a26ab
Aleksander Żak <olek1305@gmail.com>
2024-05-08 22:46:22 +0200

    Create MessageSend Event and broadcasting message

--- app/Livewire/Chat/SendMessage.php
+++ app/Livewire/Chat/SendMessage.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Chat;
 
+use App\Events\MessageSent;
 use App\Models\Conversation;
 use App\Models\Message;
 use App\Models\User;
@@ -12,8 +13,9 @@ class SendMessage extends Component
     public $selectedConversation;
     public $receiverInstance;
     public $body;
+    public $createdMessage;
 
-    protected $listeners = ['updateSendMessage'];
+    protected $listeners = ['updateSendMessage', 'dispatchMessageSent'];
 
     public function updateSendMessage(Conversation $conversation, User $receiverInstance): void
     {
@@ -28,22 +30,29 @@ class SendMessage extends Component
             return;
         }
 
-        $createdMessage = Message::create([
+        $this->createdMessage = Message::create([
             'conversation_id' => $this->selectedConversation->id,
             'sender_id' => auth()->id(),
             'receiver_id' => $this->receiverInstance->id,
             'body' => $this->body,
         ]);
 
-        $this->selectedConversation->last_time_message = $createdMessage->created_at;
+        $this->selectedConversation->last_time_message = $this->createdMessage->created_at;
         $this->selectedConversation->save();
 
-        $this->dispatch('pushMessage', $createdMessage->id)->to('chat.chatbox');
+        $this->dispatch('pushMessage', $this->createdMessage->id)->to('chat.chatbox');
 
         //refresh conversation list
         $this->dispatch('refresh')->to('chat.chat-list');
 
         $this->reset('body');
+
+        $this->dispatch('dispatchMessageSent')->self();
+    }
+
+    public function dispatchMessageSent()
+    {
+        broadcast(new MessageSent(Auth()->user(), $this->createdMessage, $this->selectedConversation, $this->receiverInstance));
     }
 
     public function render()
